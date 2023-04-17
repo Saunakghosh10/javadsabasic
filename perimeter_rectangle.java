a569113947e7cfdc3911b1698a4a3a3567d88a47
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2023-04-17 20:36:10 +0530

    commit

---
+++ perimeter_rectangle.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class perimeter_rectangle {
+    static void perimeter(int length, int breadth)
+    {
+        int perimeter = 2 * (length + breadth);
+        System.out.println("The perimeter of the given rectangle of length "
+                + length + " and breadth " + breadth + " = "
+                + perimeter);
+    }
+    public static void main(String[] args)
+    {
+        int length = 10;
+        
+        int breadth = 20;
+        perimeter(length, breadth);
+    }
+}
